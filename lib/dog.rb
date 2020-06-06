ed4c2b76fb83523f2aa6170215f7712d172a522c
David Hutchison <dhutchis68516@gmail.com>
2020-06-06 20:13:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,7 +56,7 @@ class Dog
 
   def self.find_by_id(id)
     sql = <<-SQL
-      SELECT * FROM songs WHERE id = ?
+      SELECT * FROM dogs WHERE id = ?
     SQL
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db
